d3ba32a9d3d6b89027d2ee56d70b36e86834dbd1
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2018-01-07 13:03:28 -0500

    Restructuring

---
+++ src/battery_discharge.cpp
@@ -0,0 +1,10 @@
+#include "gazebo/common/Time.hh"
+#include "gazebo/common/Assert.hh"
+#include "gazebo/common/Battery.hh"
+#include "gazebo/physics/physics.hh"
+#include <string>
+#include <functional>
+
+using namespace gazebo;
+
+
